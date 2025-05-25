70e02370842ffb7041c23b69460d0c1f7e3aa32b
Francis Craven <craven.francis@gmail.com>
2025-05-24 23:43:38 -0400

    change Response mechanism

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
 export async function GET() {
-  return NextResponse.json({ message: "Hello from API!" });
+  return Response.json({ message: "Hello from API!" })
+  ;
 }
