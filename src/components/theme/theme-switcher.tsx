9eeb6c4ec89f22a38e0bf95c43ad76d7d0860395
Francis Craven <craven.francis@gmail.com>
2025-09-18 15:58:28 -0400

    theme selecotr title

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -15,12 +15,12 @@ export function ThemeSwitcher({
   return (
     <div
       className={cn(
-        "flex h-8 items-center content-center justify-between gap-1 rounded-sm border border-border/50 bg-background/70 px-2 text-[10px] tracking-[0.08em] text-foreground/70 shadow-lg backdrop-blur-xl supports-[backdrop-filter]:bg-background/40",
+        "flex h-8 font-extrabold items-center content-center justify-between gap-1 rounded-sm border border-border/50 bg-background/70 px-2 text-[10px] tracking-[0.08em] text-foreground/70 shadow-lg backdrop-blur-xl supports-[backdrop-filter]:bg-background/40",
         className
       )}
     >
-      <label className="flex-1 text-center font-semibold" htmlFor={id}>
-        Theme{`  `}
+      <label className="flex-1 text-center" htmlFor={id}>
+        {`Vibe  `}
       </label>
       <select
         id={id}
